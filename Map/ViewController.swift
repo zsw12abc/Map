af7f6220f7ad1c38de8e4e06d0f3a5c4b3b23cbb
Shaowei <zsw22@vip.qq.com>
2015-07-18 22:17:10 +1000

    fix map

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -8,13 +8,24 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var map: MKMapView!
+    
+    var locationManage = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManage.delegate = self
+        locationManage.desiredAccuracy = kCLLocationAccuracyBest
+        locationManage.requestWhenInUseAuthorization()
+        locationManage.startUpdatingLocation()
+        
         
         let latitude:CLLocationDegrees = 40.7
         let longitude:CLLocationDegrees = -73.9
@@ -53,7 +64,21 @@ class ViewController: UIViewController, MKMapViewDelegate {
         annotation.subtitle = "Swift 2.0"
         map.addAnnotation(annotation)
     }
-
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+        var userLocation:CLLocation = locations[0] as! CLLocation
+        var latitude = userLocation.coordinate.latitude
+        var longitude = userLocation.coordinate.longitude
+        let latDelta:CLLocationDegrees = 0.01
+        let lonDelta: CLLocationDegrees = 0.01
+        
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        self.map.setRegion(region, animated: true)
+        
+    }
 
 }
 

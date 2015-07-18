85700640820560e7964bfd1d840c9d5349525f86
Shaowei <zsw22@vip.qq.com>
2015-07-18 20:34:29 +1000

    地图

---
+++ Map/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by Shaowei Zhang on 15/7/18.
+//  Copyright © 2015年 Shaowei Zhang. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var map: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let latitude:CLLocationDegrees = 40.7
+        let longitude:CLLocationDegrees = -73.9
+        let latDelta:CLLocationDegrees = 0.01
+        let lonDelta: CLLocationDegrees = 0.01
+        
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        map.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Map Pin"
+        annotation.subtitle = "Swift 2.0"
+        map.addAnnotation(annotation)
+        
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+        uilpgr.minimumPressDuration = 2
+        map.addGestureRecognizer(uilpgr)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func action(gestureRecognizer:UIGestureRecognizer){
+        print("Gesture Recongizer")
+        let touchPoint = gestureRecognizer.locationInView(self.map)
+        let newCoordinate:CLLocationCoordinate2D = map.convertPoint(touchPoint, toCoordinateFromView: self.map)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = newCoordinate
+        annotation.title = "New Place"
+        annotation.subtitle = "Swift 2.0"
+        map.addAnnotation(annotation)
+    }
+
+
+}
+
